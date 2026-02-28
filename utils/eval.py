ecd61cbbc625cc2dcc564aff8bb357f27b504bf4
Laetitia Frost <51784216+laefrost@users.noreply.github.com>
2026-02-28 08:39:37 +0100

    added pr curve

--- utils/eval.py
+++ utils/eval.py
@@ -2,6 +2,12 @@ from sklearn import metrics
 
 
 def auroc(y_true, y_score):
-    fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score)
+    fpr, tpr, thresholds = metrics.roc_curve(y_true, y_score, drop_intermediate=True)
     del thresholds
     return metrics.auc(fpr, tpr)
+
+
+def prroc(y_true, y_score):
+    ##precision, recall, thresholds = metrics.precision_recall_curve(y_true, y_score, drop_intermediate=True)
+    #del thresholds
+    return metrics.average_precision_score(y_true, y_score) #metrics.auc(recall, precision)
